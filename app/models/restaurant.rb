d4ebe88178e145cc939dbad16121be758201c4d0
Gabriel Zerbib <gabrielzerbib6@gmail.com>
2018-11-14 12:17:46 +0200

    change phone in string

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
 
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w(italian chinese japanese french belgian),
+  message: "%{value} is not a valid category" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
-   resources :restaurants
+    resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+  end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :reviews, only: [:show, :edit, :update, :destroy]
+
+    namespace :admin do
+    resources :restaurants, only: [:index]
+  end
 end
